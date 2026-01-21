f224d3586443bc90bdfb743067c8c43cdcaf9a9b
ommpatel142 <ommpatel142@gmail.com>
2026-01-21 15:05:33 -0700

    lecture3

---
+++ CMPE2300 Object Oriented Programming/demos/Lecture3/Lecture3Demo1/Lecture3Demo1/Box.cs
@@ -0,0 +1,56 @@
+﻿/*
+ *Name :Om Patel
+ *Lecture-3 Demo-1
+ *class Box will be used to illustrate properties
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3Demo1
+{
+    internal class Box
+    {
+        private int _iWidth;//baling field for the Width property 
+        private int _iHeight;
+        public int Width
+        {
+            get 
+            {
+                    return _iWidth;  
+            }
+            //if the user inputs a negative value, we'll assign the  corresponnding absolute value to the data member
+            set 
+            {
+                _iWidth = value <0 ? -value : value; 
+            }
+        }
+        public int Area
+        {
+           get{
+                return _iWidth * _iHeight;
+            }
+        }
+
+        //in this constructor, we are assigning a default value for the height parameter 
+        //Note that using the property Width in the constructor for assigning the value
+        //benefits from the validation test performed in the "set"s 
+        public Box(int width, int height = 10)
+        {
+            Width = width;
+            _iHeight = height;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"_iWidth: {_iWidth}");
+            Console.WriteLine($"Width: {Width}");
+        }
+
+        
+
+    }
+}
---
+++ CMPE2300 Object Oriented Programming/demos/Lecture3/Lecture3Demo1/Lecture3Demo1/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using Lecture3Demo1;
+
+Box b = new Box(12);
+
+
+//Get Value
+int widthValue = b.Width;
+Console.Write("get called = ");
+Console.WriteLine($"{widthValue}");
+
+Console.WriteLine();
+
+//Set Value
+b.Width = -15;
+Console.WriteLine($"set called using (b.Width) = {b.Width}");
+
+Console.WriteLine();
+
+//Display method calsed
+Console.WriteLine("Using b.Display to display the initial values of both _iWidth and Width");
+b.Display();
+
+Console.WriteLine();
+
+//Both will be called
+b.Width++;
+
+Console.WriteLine();
+
+Console.Write($"Area of b1 = {b.Area}");
+
+Console.WriteLine();
+
+Box c1 = new Box(8, 6);
+Console.WriteLine($"Area of c1 = {c1.Area}");
+
+
