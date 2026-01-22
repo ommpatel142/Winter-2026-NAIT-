54e781d8df1a1368e7a8ca9b9a24e06bc16a6d61
ommpatel142 <ommpatel142@gmail.com>
2026-01-22 14:30:18 -0700

    demo

---
+++ CMPE2300 Object Oriented Programming/demos/Lecture3/Lecture3Demo2/Lecture3Demo2/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3Demo2
+{
+    internal class Employee
+    {
+
+        public string FullName
+        {
+            get;
+            set;
+        }
+        public Employee(string name)
+        {
+
+        }
+    }
+}
---
+++ CMPE2300 Object Oriented Programming/demos/Lecture3/Lecture3Demo2/Lecture3Demo2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+/*
+ * Lecture 3 Demo2 Working with Automatic Properties
+ */
+
+using Lecture3Demo2;
+
+Employee emp = new Employee();
+
+emp.FullName = "Om";
+Console.Write($"The name of the employee is {emp.FullName}.");
