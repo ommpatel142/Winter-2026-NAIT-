f67a565a4068f5987e0abd2968041d2b23537843
ommpatel142 <ommpatel142@gmail.com>
2026-01-22 17:46:33 -0700

    demo

---
+++ CMPE1666 Intermediate Programming/demos/Lecture2B/Lecture2BDemo3/Lecture2BDemo3/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture2BDemo3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_Calculate_Btn_Click(object sender, EventArgs e)
+        {
+            int v1;
+            int v2;
+            int.TryParse(UI_Value1_Tbx.Text,out v1);
+            int.TryParse(UI_Value2_Tbx.Text,out v2);
+            int v3 = v1 + v2;
+            UI_Sum_Tbx.Text = v3.ToString() ; 
+        }
+    }
+}
---
+++ CMPE1666 Intermediate Programming/demos/Lecture2B/Lecture2BDemo3/Lecture2BDemo3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lecture2BDemo3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NAIT")]
+[assembly: AssemblyProduct("Lecture2BDemo3")]
+[assembly: AssemblyCopyright("Copyright © NAIT 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ad073269-d26e-47b7-a95f-3a29759523e5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
