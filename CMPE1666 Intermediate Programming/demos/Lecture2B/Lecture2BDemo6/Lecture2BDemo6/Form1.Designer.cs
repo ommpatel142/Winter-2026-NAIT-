9017ce7d46ae45291dae3473beb2a30dbdd017cc
ommpatel142 <ommpatel142@gmail.com>
2026-01-22 18:14:16 -0700

    demo

--- CMPE1666 Intermediate Programming/demos/Lecture2B/Lecture2BDemo6/Lecture2BDemo6/Form1.Designer.cs
+++ CMPE1666 Intermediate Programming/demos/Lecture2B/Lecture2BDemo6/Lecture2BDemo6/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.UI_Timer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // UI_Timer
@@ -38,11 +39,23 @@
             this.UI_Timer.Interval = 200;
             this.UI_Timer.Tick += new System.EventHandler(this.UI_Timer_Tick);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(136, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 132);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "STOP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(476, 397);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -52,6 +65,7 @@
         #endregion
 
         private System.Windows.Forms.Timer UI_Timer;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CMPE1666 Intermediate Programming/demos/Lecture2B/Lecture2BDemo6/Lecture2BDemo6/Form1.cs
+++ CMPE1666 Intermediate Programming/demos/Lecture2B/Lecture2BDemo6/Lecture2BDemo6/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,18 +13,25 @@ namespace Lecture2BDemo6
 {
     public partial class Form1 : Form
     {
+        Stopwatch sw = new Stopwatch();
         int count;
         public Form1()
         {
             InitializeComponent();
+            sw.Start();
             count = 0;
-            
         }
 
-        private void UI_Timer_Tick(object sender, EventArgs e)
+        //demo6 and Demo6B
+        private void UI_Timer_Tick(object sender, EventArgs e)  
         {
-            this.Text = $"Count is: {count}";
-            count++;
+            this.Text = $"Count is: {count} and number of milliseconds Elapsed: {sw.ElapsedMilliseconds}";  
+            count++;                           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sw.Stop();
         }
     }
 }
