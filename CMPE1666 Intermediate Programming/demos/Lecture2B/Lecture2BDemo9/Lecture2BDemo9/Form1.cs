7d6b8bbf9289b3ec3326edf312adfff174d6c2fe
ommpatel142 <ommpatel142@gmail.com>
2026-01-23 19:47:46 -0700

    Lecture2BDemo9

--- CMPE1666 Intermediate Programming/demos/Lecture2B/Lecture2BDemo9/Lecture2BDemo9/Form1.cs
+++ CMPE1666 Intermediate Programming/demos/Lecture2B/Lecture2BDemo9/Lecture2BDemo9/Form1.cs
@@ -15,6 +15,32 @@ namespace Lecture2BDemo9
         public Form1()
         {
             InitializeComponent();
+            UI_5_Rdb.CheckedChanged += UI_Recalculate_Event;
+            UI_7_Rdb.CheckedChanged += UI_Recalculate_Event;
+            UI_10_Rdb.CheckedChanged += UI_Recalculate_Event;
+
+        }
+        private void UI_Recalculate_Event(object sender,EventArgs e)
+        {
+            double amt;
+
+            if(!double.TryParse(UI_ItemPrice_Tbx.Text,out amt))
+            {
+                UI_Output_Tbx.Text = "";
+                return;
+            }
+
+            double tax = 0.0;
+
+            if (UI_5_Rdb.Checked)
+                tax = 0.05;
+            if (UI_7_Rdb.Checked)
+                tax = 0.07;
+            if (UI_10_Rdb.Checked)
+                tax = 0.10;
+
+            double total = amt + (amt * tax);
+            UI_Output_Tbx.Text = total.ToString("0.00");
         }
     }
 }
